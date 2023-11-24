38e8284ef5eee0e7cb02b71976a0c810f53c7dad
arafaelsmoreira <rafa4codin@gmail.com>
2023-11-24 15:55:23 +0000

    Now I Can See
    Added first simple and basic controllers/views

---
+++ mvcWebApp/mvcWebApp/Controllers/ClubController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvcWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
